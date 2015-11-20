d12249cde2373a8c61dbfe5a04fe7a0318d67687
KFL <kfldev@outlook.com>
2015-11-20 11:50:20 -0800

    Fix symlink creation bug
    When linking the atom settings git repo to
    ~/.atom, `mklink` need to specify `/D` so
    the link is of type `Directory`. Otherwise
    it's defaulted to `file`.

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 ﻿param([switch]$skip)
 
 cmd /c mklink $home\.tern-config $PSScriptRoot\.tern-config
-cmd /c mklink $home\.atom $PSScriptRoot
+cmd /c mklink /D $home\.atom $PSScriptRoot
 if (-not $skip)
 {
     apm install --packages-file $PSScriptRoot\packages.txt
