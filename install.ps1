3c1768d22af4aacb93a88394ae6dc0f371753c68
KFL <kfldev@outlook.com>
2015-11-29 01:03:57 -0800

    remove before mklink

--- install.ps1
+++ install.ps1
@@ -1,8 +1,14 @@
 ﻿param([switch]$skip)
 
 npm i -g auto-apm
+
+rm -force $home\.tern-config
 cmd /c mklink $home\.tern-config $PSScriptRoot\.tern-config
+
+# it requires two consecutive remove-item to remove the directory
+cmd /c rmdir /s /q $home\.atom
 cmd /c mklink /D $home\.atom $PSScriptRoot
+
 if (-not $skip)
 {
     auto-apm -p $PSScriptRoot\packages.json
